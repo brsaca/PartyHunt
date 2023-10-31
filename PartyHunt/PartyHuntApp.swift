000dfe3f29b23aea3443b7bd56244a1a8e92456c
brsaca <brsaca@gmail.com>
2023-10-30 18:50:14 -0600

    Initial Commit

---
+++ PartyHunt/PartyHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyHuntApp.swift
+//  PartyHunt
+//
+//  Created by Brenda Saavedra Cantu on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PartyHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
