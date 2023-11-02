9a4f1b00443a4954c817ed627153cec38bc7e8b9
brsaca <brsaca@gmail.com>
2023-11-01 19:10:08 -0600

    feat: add ViewModel in the environment

--- PartyHunt/App/PartyHuntApp.swift
+++ PartyHunt/App/PartyHuntApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct PartyHuntApp: App {
+    // MARK: View Properties
+    @State var viewModel = EventViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ListView()
+                .environment(viewModel)
         }
     }
 }
